d4c37b11377bef6b3f97d0c971f0230f115607e3
Sergi Guimerà Roig <sergiguimeraroig@gmail.com>
2024-10-11 21:37:22 +0200

    roman numbers

--- src/firstClass_p18.clj
+++ src/firstClass_p18.clj
@@ -5,7 +5,7 @@
     ((comp (partial apply +) (partial filter odd?) (partial map count)) lst)
   )
 
-(println "fn1 4:" (fn21 '("a" "bb" "ccc")))
+(println "fn21 4:" (fn21 '("a" "bb" "ccc")))
 
 (println)
 
@@ -15,3 +15,27 @@
 )
 
 (println "fn22: (1 4 16 25 25):" (fn22 '(1 2 3 4 0 5 6 5 5 5 5 5 5 5)))
+(println)
+
+;5
+(defn fn25 [s]
+  (let [roman-to-int {\I 1 \V 5 \X 10 \L 50 \C 100 \D 500 \M 1000}
+        values (map roman-to-int s)]
+    (reduce 
+     (fn [acc [curr  nextt]]
+       (if (and nextt (< curr nextt))
+         (- acc curr)
+         (+ acc curr)
+         )
+       )
+     0
+     (partition 2 1 (concat values [0]))
+     ) 
+    )
+  )
+
+(println "fn 25: 3 -->" (fn25 "III"))
+(println "fn 25: 6 -->" (fn25 "VI"))
+(println "fn 25: 4 -->" (fn25 "IV"))
+(println "fn 25: 68 -->" (fn25 "LXVIII"))
+(println "fn 25: 1994 -->" (fn25 "MCMXCIV"))
